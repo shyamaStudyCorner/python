298e3773920cb59b1ce479b6ebe132e8a471ee68
shyamaStudyCorner <94749678+shyamaStudyCorner@users.noreply.github.com>
2021-11-20 19:56:28 +0530

    Add files via upload

---
+++ da3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.randn(10, 4),
+      index = pd.date_range('1/1/2000', periods=10),
+      columns = ['A', 'B', 'C', 'D'])
+print (df)
+r = df.rolling(window=3,min_periods=1)
+print (r[['A','B']].aggregate(np.sum))
